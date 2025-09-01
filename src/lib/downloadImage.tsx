2c3081a2d2dc881f01f041ec8629ba9ad6deb60d
Davematteer <davematteer@gmail.com>
2025-09-01 15:22:48 +0000

    fixing deployments

--- src/lib/downloadImage.tsx
+++ src/lib/downloadImage.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-
 import * as htmlToImage from "html-to-image";
-
 
 export function DownloadCardButton({ targetId }: { targetId: string }) {
   const handleDownload = async () => {
@@ -10,17 +8,24 @@ export function DownloadCardButton({ targetId }: { targetId: string }) {
     if (!node) return;
 
     try {
-      const dataUrl = await htmlToImage.toPng(node,{ pixelRatio:6, fontEmbedCSS: "font-sans"});
-      
-      if (/Mobi|Android/i.test(navigator.userAgent)) {
-        window.open(dataUrl, "_blank"); // open in new tab on mobile
+      const dataUrl = await htmlToImage.toPng(node, { pixelRatio: 6, fontEmbedCSS: "font-sans" });
+
+      // Detect iOS Safari
+      const isIosSafari =
+        /iP(hone|od|ad)/.test(navigator.userAgent) &&
+        /Safari/.test(navigator.userAgent) &&
+        !/Chrome|CriOS|FxiOS|OPiOS|EdgiOS/.test(navigator.userAgent);
+
+      if (/Mobi|Android/i.test(navigator.userAgent) || isIosSafari) {
+        // open in new tab on mobile or iOS Safari
+        window.open(dataUrl, "_blank");
       } else {
         const link = document.createElement("a");
         link.href = dataUrl;
-        link.download = `${targetId}.png`;
+        //link.download = `${targetId}.png`;
         link.click();
       }
-      
+
     } catch (err) {
       console.error("Failed to capture node:", err);
     }
