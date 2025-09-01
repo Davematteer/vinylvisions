60d14aeb20224a10b65c361acd5b7e477392e1b7
Davematteer <davematteer@gmail.com>
2025-09-01 15:51:14 +0000

    fixing deployments

--- src/lib/downloadImage.tsx
+++ src/lib/downloadImage.tsx
@@ -1,87 +1,40 @@
-// "use client";
-
-// import * as htmlToImage from "html-to-image";
-
-// export function DownloadCardButton({ targetId }: { targetId: string }) {
-//   const handleDownload = async () => {
-//     const node = document.getElementById(targetId);
-//     if (!node) return;
-
-//     try {
-//       const dataUrl = await htmlToImage.toPng(node, { pixelRatio: 6, fontEmbedCSS: "font-sans" });
-
-//       // Detect iOS Safari
-//       const isIosSafari =
-//         /iP(hone|od|ad)/.test(navigator.userAgent) &&
-//         /Safari/.test(navigator.userAgent) &&
-//         !/Chrome|CriOS|FxiOS|OPiOS|EdgiOS/.test(navigator.userAgent);
-
-//       if (/Mobi|Android/i.test(navigator.userAgent) || isIosSafari) {
-//         // open in new tab on mobile or iOS Safari
-//         window.open(dataUrl, "_blank");
-//       } else {
-//         const link = document.createElement("a");
-//         link.href = dataUrl;
-//       link.download = `${targetId}.png`;
-//         link.click();
-//       }
-
-//     } catch (err) {
-//       console.error("Failed to capture node:", err);
-//     }
-//   };
-
-//   return (
-//     <button
-//       onClick={handleDownload}
-//       className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800"
-//     >
-//       Download Preview
-//     </button>
-//   );
-// }
-
 "use client";
-import React, { useCallback } from 'react';
-import { toPng } from 'html-to-image';
 
-export function DownloadCardButton({ targetId }: { targetId: string }) {
-  const handleDownload = useCallback(() => {
-    const node = document.getElementById(targetId);
-    if (node === null) {
-      return;
+import * as htmlToImage from "html-to-image";
+
+export const handleDownload = async (targetId:string) => {
+  const node = document.getElementById(targetId);
+  if (!node) return;
+
+  try {
+    const dataUrl = await htmlToImage.toPng(node, { pixelRatio: 6, fontEmbedCSS: "font-sans" });
+
+    // Detect iOS Safari
+    const isIosSafari =
+      /iP(hone|od|ad)/.test(navigator.userAgent) &&
+      /Safari/.test(navigator.userAgent) &&
+      !/Chrome|CriOS|FxiOS|OPiOS|EdgiOS/.test(navigator.userAgent);
+
+    if (/Mobi|Android/i.test(navigator.userAgent) || isIosSafari) {
+      // open in new tab on mobile or iOS Safari
+      window.open(dataUrl, "_blank");
+    } else {
+      const link = document.createElement("a");
+      link.href = dataUrl;
+      link.download = `${targetId}.png`;
+      link.click();
     }
 
-    toPng(node, { 
-      cacheBust: true,
-      pixelRatio: 6, 
-      fontEmbedCSS: "font-sans" 
-    })
-      .then((dataUrl) => {
-        // Detect iOS Safari
-        const isIosSafari =
-          /iP(hone|od|ad)/.test(navigator.userAgent) &&
-          /Safari/.test(navigator.userAgent) &&
-          !/Chrome|CriOS|FxiOS|OPiOS|EdgiOS/.test(navigator.userAgent);
+  } catch (err) {
+    console.error("Failed to capture node:", err);
+  }
+};
 
-        if (/Mobi|Android/i.test(navigator.userAgent) || isIosSafari) {
-          // open in new tab on mobile or iOS Safari
-          window.open(dataUrl, "_blank");
-        } else {
-          const link = document.createElement("a");
-          link.href = dataUrl;
-          //link.download = `${targetId}.png`;
-          link.click();
-        }
-      })
-      .catch((err) => {
-        console.error("Failed to capture node:", err);
-      });
-  }, [targetId]);
+export function DownloadCardButton({ targetId }: { targetId: string }) {
 
   return (
     <button
-      onClick={handleDownload}
+      onClick={() => handleDownload(targetId)}
       className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800"
     >
       Download Preview
