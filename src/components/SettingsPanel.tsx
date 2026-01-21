4c1661c975b10b3dc9f2f9d1cf0e6f3a61fc473d
Davematteer <davematteer@gmail.com>
2026-01-21 18:40:49 +0000

    adding customizaiton

--- src/components/SettingsPanel.tsx
+++ src/components/SettingsPanel.tsx
@@ -72,7 +72,7 @@ export function SettingsPanel({canvas}:{canvas:Canvas|null}){
   const addImage = () =>{
       if (!canvas) return;
 
-       FabricImage.fromURL('/covers/FYN.png').then((img) => {
+       FabricImage.fromURL('/covers/testImage2.jpg').then((img) => {
         // Scale the image to fit nicely
         img.scaleToWidth(200);
         img.set({
